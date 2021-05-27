59ca7bd14842c09cd6768e52f52b0cbde9327a12
Adiel Ferreira <adiel.ferreira26@gmail.com>
2021-05-27 17:42:20 -0300

    update loading screen

--- app/src/main/java/com/example/appteste/DisplayMessageActivity.kt
+++ app/src/main/java/com/example/appteste/DisplayMessageActivity.kt
@@ -7,7 +7,7 @@ import android.widget.TextView
 class DisplayMessageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_display_message)
+        setContentView(R.layout.loading_screen)
 
         // Get the Intent that started this activity and extract the string
         val message = intent.getStringExtra(EXTRA_MESSAGE)
